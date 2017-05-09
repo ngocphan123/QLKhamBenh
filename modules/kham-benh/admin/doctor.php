09b879e5b89744e20ff693678b326d76760feac8
VINADES.,JSC <contact@vinades.vn>
2017-05-09 15:34:52 +0700

    thêm funtion bác sĩ trong admin

---
+++ modules/kham-benh/admin/doctor.php
@@ -0,0 +1,235 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2017 VINADES.,JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Tue, 09 May 2017 08:32:25 GMT
+ */
+
+if ( ! defined( 'NV_IS_FILE_ADMIN' ) ) die( 'Stop!!!' );
+
+if ( $nv_Request->isset_request( 'delete_id', 'get' ) and $nv_Request->isset_request( 'delete_checkss', 'get' ))
+{
+	$id = $nv_Request->get_int( 'delete_id', 'get' );
+	$delete_checkss = $nv_Request->get_string( 'delete_checkss', 'get' );
+	if( $id > 0 and $delete_checkss == md5( $id . NV_CACHE_PREFIX . $client_info['session_id'] ) )
+	{
+		$db->query('DELETE FROM ' . NV_PREFIXLANG . '_' . $module_data . '_doctor  WHERE id = ' . $db->quote( $id ) );
+		$nv_Cache->delMod( $module_name );
+		Header( 'Location: ' . NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name . '&' . NV_OP_VARIABLE . '=' . $op );
+		die();
+	}
+}
+
+$row = array();
+$error = array();
+$row['id'] = $nv_Request->get_int( 'id', 'post,get', 0 );
+if ( $nv_Request->isset_request( 'submit', 'post' ) )
+{
+	$row['name'] = $nv_Request->get_title( 'name', 'post', '' );
+	$row['datetime'] = $nv_Request->get_int( 'datetime', 'post', 0 );
+	$row['specialist_id'] = $nv_Request->get_int( 'specialist_id', 'post', 0 );
+	$row['position'] = $nv_Request->get_title( 'position', 'post', '' );
+	$row['address'] = $nv_Request->get_title( 'address', 'post', '' );
+	$row['phone'] = $nv_Request->get_int( 'phone', 'post', 0 );
+	$row['business'] = $nv_Request->get_title( 'business', 'post', '' );
+	$row['story'] = $nv_Request->get_string( 'story', 'post', '' );
+
+	if( empty( $row['name'] ) )
+	{
+		$error[] = $lang_module['error_required_name'];
+	}
+	elseif( empty( $row['specialist_id'] ) )
+	{
+		$error[] = $lang_module['error_required_specialist_id'];
+	}
+	elseif( empty( $row['position'] ) )
+	{
+		$error[] = $lang_module['error_required_position'];
+	}
+
+	if( empty( $error ) )
+	{
+		try
+		{
+			if( empty( $row['id'] ) )
+			{
+				$stmt = $db->prepare( 'INSERT INTO ' . NV_PREFIXLANG . '_' . $module_data . '_doctor (name, datetime, specialist_id, position, address, phone, business, story) VALUES (:name, :datetime, :specialist_id, :position, :address, :phone, :business, :story)' );
+			}
+			else
+			{
+				$stmt = $db->prepare( 'UPDATE ' . NV_PREFIXLANG . '_' . $module_data . '_doctor SET name = :name, datetime = :datetime, specialist_id = :specialist_id, position = :position, address = :address, phone = :phone, business = :business, story = :story WHERE id=' . $row['id'] );
+			}
+			$stmt->bindParam( ':name', $row['name'], PDO::PARAM_STR );
+			$stmt->bindParam( ':datetime', $row['datetime'], PDO::PARAM_INT );
+			$stmt->bindParam( ':specialist_id', $row['specialist_id'], PDO::PARAM_INT );
+			$stmt->bindParam( ':position', $row['position'], PDO::PARAM_STR );
+			$stmt->bindParam( ':address', $row['address'], PDO::PARAM_STR );
+			$stmt->bindParam( ':phone', $row['phone'], PDO::PARAM_INT );
+			$stmt->bindParam( ':business', $row['business'], PDO::PARAM_STR );
+			$stmt->bindParam( ':story', $row['story'], PDO::PARAM_STR, strlen($row['story']) );
+
+			$exc = $stmt->execute();
+			if( $exc )
+			{
+				$nv_Cache->delMod( $module_name );
+				Header( 'Location: ' . NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name . '&' . NV_OP_VARIABLE . '=' . $op );
+				die();
+			}
+		}
+		catch( PDOException $e )
+		{
+			trigger_error( $e->getMessage() );
+			die( $e->getMessage() ); //Remove this line after checks finished
+		}
+	}
+}
+elseif( $row['id'] > 0 )
+{
+	$row = $db->query( 'SELECT * FROM ' . NV_PREFIXLANG . '_' . $module_data . '_doctor WHERE id=' . $row['id'] )->fetch();
+	if( empty( $row ) )
+	{
+		Header( 'Location: ' . NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name . '&' . NV_OP_VARIABLE . '=' . $op );
+		die();
+	}
+}
+else
+{
+	$row['id'] = 0;
+	$row['name'] = '';
+	$row['datetime'] = 0;
+	$row['specialist_id'] = 0;
+	$row['position'] = '';
+	$row['address'] = '';
+	$row['phone'] = 0;
+	$row['business'] = '';
+	$row['story'] = '';
+}
+$array_specialist_id_kham_benh = array();
+$_sql = 'SELECT id,name_specialist FROM nv4_vi_kham_benh_specialist';
+$_query = $db->query( $_sql );
+while( $_row = $_query->fetch() )
+{
+	$array_specialist_id_kham_benh[$_row['id']] = $_row;
+}
+
+
+$q = $nv_Request->get_title( 'q', 'post,get' );
+
+// Fetch Limit
+$show_view = false;
+if ( ! $nv_Request->isset_request( 'id', 'post,get' ) )
+{
+	$show_view = true;
+	$per_page = 20;
+	$page = $nv_Request->get_int( 'page', 'post,get', 1 );
+	$db->sqlreset()
+		->select( 'COUNT(*)' )
+		->from( '' . NV_PREFIXLANG . '_' . $module_data . '_doctor' );
+
+	if( ! empty( $q ) )
+	{
+		$db->where( 'name LIKE :q_name OR datetime LIKE :q_datetime OR specialist_id LIKE :q_specialist_id OR position LIKE :q_position OR address LIKE :q_address OR phone LIKE :q_phone OR business LIKE :q_business' );
+	}
+	$sth = $db->prepare( $db->sql() );
+
+	if( ! empty( $q ) )
+	{
+		$sth->bindValue( ':q_name', '%' . $q . '%' );
+		$sth->bindValue( ':q_datetime', '%' . $q . '%' );
+		$sth->bindValue( ':q_specialist_id', '%' . $q . '%' );
+		$sth->bindValue( ':q_position', '%' . $q . '%' );
+		$sth->bindValue( ':q_address', '%' . $q . '%' );
+		$sth->bindValue( ':q_phone', '%' . $q . '%' );
+		$sth->bindValue( ':q_business', '%' . $q . '%' );
+	}
+	$sth->execute();
+	$num_items = $sth->fetchColumn();
+
+	$db->select( '*' )
+		->order( 'id DESC' )
+		->limit( $per_page )
+		->offset( ( $page - 1 ) * $per_page );
+	$sth = $db->prepare( $db->sql() );
+
+	if( ! empty( $q ) )
+	{
+		$sth->bindValue( ':q_name', '%' . $q . '%' );
+		$sth->bindValue( ':q_datetime', '%' . $q . '%' );
+		$sth->bindValue( ':q_specialist_id', '%' . $q . '%' );
+		$sth->bindValue( ':q_position', '%' . $q . '%' );
+		$sth->bindValue( ':q_address', '%' . $q . '%' );
+		$sth->bindValue( ':q_phone', '%' . $q . '%' );
+		$sth->bindValue( ':q_business', '%' . $q . '%' );
+	}
+	$sth->execute();
+}
+
+
+$xtpl = new XTemplate( $op . '.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file );
+$xtpl->assign( 'LANG', $lang_module );
+$xtpl->assign( 'NV_LANG_VARIABLE', NV_LANG_VARIABLE );
+$xtpl->assign( 'NV_LANG_DATA', NV_LANG_DATA );
+$xtpl->assign( 'NV_BASE_ADMINURL', NV_BASE_ADMINURL );
+$xtpl->assign( 'NV_NAME_VARIABLE', NV_NAME_VARIABLE );
+$xtpl->assign( 'NV_OP_VARIABLE', NV_OP_VARIABLE );
+$xtpl->assign( 'MODULE_NAME', $module_name );
+$xtpl->assign( 'MODULE_UPLOAD', $module_upload );
+$xtpl->assign( 'NV_ASSETS_DIR', NV_ASSETS_DIR );
+$xtpl->assign( 'OP', $op );
+$xtpl->assign( 'ROW', $row );
+
+foreach( $array_specialist_id_kham_benh as $value )
+{
+	$xtpl->assign( 'OPTION', array(
+		'key' => $value['id'],
+		'title' => $value['name_specialist'],
+		'selected' => ($value['id'] == $row['specialist_id']) ? ' selected="selected"' : ''
+	) );
+	$xtpl->parse( 'main.select_specialist_id' );
+}
+$xtpl->assign( 'Q', $q );
+
+if( $show_view )
+{
+	$base_url = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=' . $op;
+	if( ! empty( $q ) )
+	{
+		$base_url .= '&q=' . $q;
+	}
+	$generate_page = nv_generate_page( $base_url, $num_items, $per_page, $page );
+	if( !empty( $generate_page ) )
+	{
+		$xtpl->assign( 'NV_GENERATE_PAGE', $generate_page );
+		$xtpl->parse( 'main.view.generate_page' );
+	}
+	$number = $page > 1 ? ($per_page * ( $page - 1 ) ) + 1 : 1;
+	while( $view = $sth->fetch() )
+	{
+		$view['number'] = $number++;
+		$view['specialist_id'] = $array_specialist_id_kham_benh[$view['specialist_id']]['name_specialist'];
+		$view['link_edit'] = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=' . $op . '&amp;id=' . $view['id'];
+		$view['link_delete'] = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=' . $op . '&amp;delete_id=' . $view['id'] . '&amp;delete_checkss=' . md5( $view['id'] . NV_CACHE_PREFIX . $client_info['session_id'] );
+		$xtpl->assign( 'VIEW', $view );
+		$xtpl->parse( 'main.view.loop' );
+	}
+	$xtpl->parse( 'main.view' );
+}
+
+
+if( ! empty( $error ) )
+{
+	$xtpl->assign( 'ERROR', implode( '<br />', $error ) );
+	$xtpl->parse( 'main.error' );
+}
+
+$xtpl->parse( 'main' );
+$contents = $xtpl->text( 'main' );
+
+$page_title = $lang_module['doctor'];
+
+include NV_ROOTDIR . '/includes/header.php';
+echo nv_admin_theme( $contents );
+include NV_ROOTDIR . '/includes/footer.php';
--- modules/kham-benh/language/admin_vi.php
+++ modules/kham-benh/language/admin_vi.php
@@ -21,95 +21,8 @@ $lang_module['config'] = 'Cấu hình';
 $lang_module['save'] = 'Save';
 $lang_module['search'] = 'Tìm kiếm';
 
-//lang cho can chi
-$lang_module['ty'] = 'Tý';
-$lang_module['suu'] = 'Sửu';
-$lang_module['dan'] = 'Dần';
-$lang_module['mao'] = 'Mao';
-$lang_module['thin'] = 'Thìn';
-$lang_module['ti'] = 'Tỵ';
-$lang_module['ngo'] = 'Ngọ';
-$lang_module['mui'] = 'Mùi';
-$lang_module['than'] = 'Thân';
-$lang_module['dau'] = 'Dậu';
-$lang_module['tuat'] = 'Tuất';
-$lang_module['hoi'] = 'Hợi';
-$lang_module['giap'] = 'Giáp';
-$lang_module['at'] = 'Ất';
-$lang_module['binh'] = 'Bính';
-$lang_module['dinh'] = 'Đinh';
-$lang_module['mau'] = 'Mậu';
-$lang_module['ky'] = 'Kỷ';
-$lang_module['canh'] = 'Canh';
-$lang_module['tan'] = 'Tân';
-$lang_module['nham'] = 'Nhâm';
-$lang_module['quy'] = 'Quý';
-
-$lang_module['sim_comment'] = 'Bình giảng sim';
-
-//Lang for function provider_network
-$lang_module['provider_network'] = 'Nhà mạng';
-$lang_module['add'] = 'Thêm mới';
-$lang_module['edit'] = 'Sửa';
-$lang_module['delete'] = 'Xóa';
-$lang_module['number'] = 'STT';
-$lang_module['active'] = 'Hiển thị';
-$lang_module['title'] = 'Tên nhà mạng';
-$lang_module['begin_number'] = 'Đầu số';
-$lang_module['photo'] = 'Ảnh minh họa';
-$lang_module['error_required_title'] = 'Lỗi: bạn cần nhập dữ liệu cho Tên nhà mạng';
-
-//Lang for function provider_network
-$lang_module['search_title'] = 'Nhập từ khóa tìm kiếm';
-$lang_module['search_submit'] = 'Tìm kiếm';
-$lang_module['input_keyword_tags'] = 'Nhập danh sách các đầu số...';
-
-//Lang for function provider_sim
-$lang_module['provider_sim'] = 'Nhà cung cấp';
-$lang_module['provider_sim_name'] = 'Tên nhà cung cấp';
-$lang_module['provider_code'] = 'Mã nhà cung cấp';
-$lang_module['contact_user'] = 'Tên người liên hệ';
-$lang_module['phone'] = 'Số điện thoại';
-$lang_module['email'] = 'Email';
-$lang_module['address'] = 'Địa chỉ';
-$lang_module['website'] = 'Website';
-$lang_module['error_required_provider_code'] = 'Lỗi: bạn cần nhập dữ liệu cho Mã nhà cung cấp';
-$lang_module['error_required_phone'] = 'Lỗi: bạn cần nhập dữ liệu cho Số điện thoại';
-$lang_module['error_required_email'] = 'Lỗi: bạn cần nhập dữ liệu cho Email';
-
-//Lang for function group_vip
-$lang_module['group_vip'] = 'Nhóm sim VIP';
-$lang_module['name_group'] = 'Tên nhóm';
-
-//Lang for function sim_list
-$lang_module['sim_list'] = 'Quản lí sim';
-$lang_module['sim_code'] = 'Mã sim';
-$lang_module['sim_number'] = 'Số điện thoại';
-$lang_module['group_vip_id'] = 'Nhóm VIP';
-$lang_module['sim_price'] = 'Giá bán';
-$lang_module['addtime'] = 'Thời gian đăng';
-$lang_module['edittime'] = 'Thời gian sửa';
-$lang_module['status'] = 'Hiển thị';
-$lang_module['hot_config'] = 'Sim hot';
-$lang_module['discount_money'] = 'Tiền giảm giá';
-$lang_module['begintime_discount'] = 'Giảm giá từ ngày';
-$lang_module['endtime_discount'] = 'Ngày hết hạn giảm giá';
-$lang_module['provider_id'] = 'Nhà phân phối';
-$lang_module['admin_id_post'] = 'Người đăng';
-$lang_module['sell_status'] = 'Tình trạng';
-$lang_module['properties_config'] = 'Các thuộc tính hiển thị';
-$lang_module['sim_note'] = 'Ghi chú về sim';
-$lang_module['provider_network_id'] = 'Nhà mạng';
-$lang_module['error_required_sim_code'] = 'Lỗi: bạn cần nhập dữ liệu cho Mã sim';
-$lang_module['error_required_sim_number'] = 'Lỗi: bạn cần nhập dữ liệu cho Số điện thoại';
-$lang_module['error_number_phone'] = 'Lỗi: số điện thoại chưa đúng';
-$lang_module['error_required_sim_price'] = 'Lỗi: bạn cần nhập dữ liệu cho Giá bán';
-
-//Lang for function search
-$lang_module['number_phone'] = 'Số điện thoại';
-
 //Lang for function doctor
-$lang_module['doctor'] = 'doctor';
+$lang_module['doctor'] = 'Bác sĩ';
 $lang_module['name'] = 'Họ&tên bác sĩ';
 $lang_module['datetime'] = 'Ngày tháng năm sinh';
 $lang_module['specialist'] = 'Chuyên khoa';
@@ -121,3 +34,9 @@ $lang_module['story'] = 'Tiểu sử';
 $lang_module['name_specialist'] = 'Chuyên khoa';
 $lang_module['description'] = 'Giới thiệu';
 $lang_module['error_required_name_specialist'] = 'Lỗi: bạn cần nhập dữ liệu cho Chuyên khoa';
+
+//Lang for function doctor
+$lang_module['specialist_id'] = 'Chuyên khoa';
+$lang_module['error_required_name'] = 'Lỗi: bạn cần nhập dữ liệu cho Họ&tên bác sĩ';
+$lang_module['error_required_specialist_id'] = 'Lỗi: bạn cần nhập dữ liệu cho Chuyên khoa';
+$lang_module['error_required_position'] = 'Lỗi: bạn cần nhập dữ liệu cho Chức vụ';
